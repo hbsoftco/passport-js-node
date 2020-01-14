fed2bd10bf4027adffcc4ba037f9d41656a9812b
hbsoftco <hossein.bajan@gmail.com>
2020-01-15 01:44:19 +0330

    First commit

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    google: {
+        clientID: '917169610920-17oikmv1qp5hp2t5rk8s7horp38b2s09.apps.googleusercontent.com',
+        clientSecret: 'tklySIHCyw3RotFhfjklwCEZ'
+    },
+    mongodb: {
+        dbURI: 'mongodb://hbsoft:hosseinhb69@ds361968.mlab.com:61968/passport'
+    }
+};
---
+++ config/passport-setup.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./keys');
+const User = require('../models/user-model');
+
+passport.use(
+    new GoogleStrategy({
+        // options for the google strategy
+        callbackURL: '/auth/google/redirect',
+        clientID: keys.google.clientID,
+        clientSecret: keys.google.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        // checkif user already exist
+        User.findOne({ googleId: profile.id }).then((currentUser) => {
+            if (currentUser) {
+                // already have the user
+                console.log('user is: ' + currentUser);
+
+            } else {
+                // if not, create new user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id,
+                }).save().then((newUser) => {
+                    console.log('new user created:' + newUser);
+                });
+            }
+        });
+
+
+
+
+
+    })
+);
