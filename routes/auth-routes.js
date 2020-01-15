c9a2948395193ba0e827def9b3fe16850bb7ae55
hbsoftco <hossein.bajan@gmail.com>
2020-01-15 09:39:38 +0330

    Add profile section

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,28 +1,29 @@
-const router = require('express').Router();
-const passport = require('passport');
+const router = require("express").Router();
+const passport = require("passport");
 
 // auth login
 router.get('/login', (req, res) => {
-    res.render('login');
+  res.render('login', { user: req.user });
 });
 
 // auth logout
-router.get('/logout', (req, res) => {
-    // handle with passport
-    res.send('logging out');
+router.get("/logout", (req, res) => {
+  // handle with passport
+  res.send("logging out");
 });
 
 // auth with google
-router.get('/google', passport.authenticate('google', {
-    scope: ['profile']
-}));
+router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: ["profile"]
+  })
+);
 
 // callback route for google to redirect to
-router.get('/google/redirect',
-    passport.authenticate('google'),
-    (req, res) => {
-        // Successful authentication, redirect home.
-        res.redirect('/');
-    });
+router.get("/google/redirect", passport.authenticate("google"), (req, res) => {
+  // Successful authentication, redirect home.
+  res.redirect('/profile');
+});
 
 module.exports = router;
